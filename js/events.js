f2faf801241ae45ba6be6bf4edf5c967ab113eca
Andrew Sprague <andrewsprague@outlook.com>
2018-07-21 03:39:04 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,36 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function () {
+    alert('Hey!');
+  });
+  
+  return;
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if(key.which ===  71 ) {
+      alert('You pressed G!');
+    }
+    return;
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+    return
+  });
+}
+
+
 $(document).ready(function(){
 
 // call functions here
